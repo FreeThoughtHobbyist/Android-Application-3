7b2337460ed52ba0bf09ff9c8b66a95e3eb5a986
bboudra <benboudra@gmail.com>
2016-02-23 11:56:35 -0700

    initialized the Repository

---
+++ app/src/test/java/com/example/benjamin/android_application_3/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.benjamin.android_application_3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
